01c0eba236a0986ff2fe0b28f4407332a072f6a3
nora von breitenbach <noravonbreitenbach@gmail.com>
2021-07-28 19:40:57 +0200

    new landing

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,10 @@
+class PagesController < ApplicationController
+  def about
+  end
+  def team
+  end
+  def tech
+  end
+  def impressum
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :user_signups, only: [:new, :create]
 root 'user_signups#new'
+
+get 'about', to: 'pages#about'
+get 'team', to: 'pages#team'
+get 'tech', to: 'pages#tech'
+get 'impressum', to: 'pages#impressum'
 end
