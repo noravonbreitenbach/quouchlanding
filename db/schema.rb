d2f4ac6048672111aab4a884d5642a90e3cbfbb6
nora von breitenbach <noravonbreitenbach@gmail.com>
2021-09-11 15:22:15 +0200

    stuff

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_07_14_153728) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "user_signups", force: :cascade do |t|
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
